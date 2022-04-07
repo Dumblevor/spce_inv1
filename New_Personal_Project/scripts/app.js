e31dbfd8bcca5b0663c668c30322f91e4da668e5
Dimitar <dimitar.vidolov@gmail.com>
2022-04-07 20:34:12 +0300

    starting over

--- New_Personal_Project/scripts/app.js
+++ New_Personal_Project/scripts/app.js
@@ -1,35 +1,19 @@
-gridSize = 1000;
-const alienline1 = document.getElementsByClassName("aliensLine1");
+const grid = document.querySelector('.grid');
+const width = 25;
+const height = 40;
+const cellCount = width * height;
+let playerPosit = 1000 - 12;
+let enemyPosit = 1;
+let cells = [];
 
-// for (let i = 1; i < 6; i++) {
-//   newDiv = document.createElement("div");
-//   newImg = document.createElement("img");
-//   newImg.src = '/assets/invaderType1.png';
-//   newDiv.classList.add('alienclass1');
-//   newDiv.classList.add('alive');
-//   newDiv.setAttribute('id','alien' + i);
-//   newDiv.appendChild(newImg);
-//   dalienline1.appendChild(newDiv);
-// }
-
-
-
+function createGrid() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElemet('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+    cells.push(cell);
 
 
+  }
 
-// document.body.onload = addElement;
-
-// function addElement () {
-//   // create a new div element
-//   const newDiv = document.createElement("div");
-
-//   // and give it some content
-//   const newContent = document.createTextNode("Hi there and greetings!");
-
-//   // add the text node to the newly created div
-//   newDiv.appendChild(newContent);
-
-//   // add the newly created element and its content into the DOM
-//   const currentDiv = document.getElementById("div1");
-//   document.body.insertBefore(newDiv, currentDiv);
-// }
+}
