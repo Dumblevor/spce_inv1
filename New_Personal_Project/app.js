01a66fa561a795b8266623031bca78913541af54
Dimitar <dimitar.vidolov@gmail.com>
2022-04-22 14:41:41 +0300

    clean up, styling

--- New_Personal_Project/app.js
+++ New_Personal_Project/app.js
@@ -2,15 +2,21 @@ const grid = document.querySelector('.grid');
 const scoreBoard = document.querySelector('.scoreBoard');
 const levelNum = document.querySelector('.levelNum');
 const playerNameHtml = document.querySelector('.playerName');
+const modal = document.getElementById("myModal");
+let button1 = document.querySelector('.button1');
+const span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+const modalForm = document.querySelector(".modalform");
+const submit = document.querySelector(".submit");
 const width = 20;
+let gameOver = 0;
+let bombsTiming = 500;
 const height = width;
 const cellCount = width * height;
 let playerPosit = cellCount - width * 3 / 2; //bottom screen, middle;
 let speedPos = playerPosit + width;
 let cells = [];
 let currentLevel = 1;
-let playerOneScore = 0; //keeps track of his score
-// let playerCurrentDamage = 10; //player damage per hit of 1 laser
+let playerOneScore = 0; //keeps track of player 1 score
 let playerCurrentHealth = 1; //player outstanding health points
 let playerLaserLoc = playerPosit;
 let nextLaserLoc = playerLaserLoc - width;
@@ -21,36 +27,13 @@ let aliensPosArray3 = [];
 let alienBombPosArray = [];
 let rocksPositions = [];
 scoreBoard.innerHTML = playerOneScore;
-let gameOver = 0;
-let bombsTiming = 500;
-// let speedTest = document.querySelector(".speed");
 
-
-// function loopSpeedLines(x) {
-//   cells[speedPos].classList.remove('speed');
-//   speedTest.style.backgroundImage = "";
-// let speedTest = document.querySelector(".speed");
-// let i = 1;
-// if (x = 0) { 
-// let speedLoop = setInterval(() => {
-//   document.style.backgroundImage = "url('assets/" + i + ".png')";
-//   i++;
-//   if (i > 10) {
-//     i = 1;
-//   }
-//  }, 150)
-// } else {
-//   clearInterval();
-//   console.log('asdasd');
-//   let y = 1;
-//   loopSpeedLines(y);
-// }
-// }
 
 function updateLvlOnStart (x) {
   currentLevel = x;
   levelNum.innerHTML = x;
 }
+
 function bombsTimingUpdate (levelX) {  
   bombsTiming = bombsTiming * ((100  - (10 * levelX))/100) ; //boosts bombing speed on every level (by lowering the interval)
 }
@@ -62,9 +45,7 @@ function damagePlayer() {
     playerCurrentHealth -= 1;
     playerCurrentHealth == 0 ? gameOver = 1 : gameOver = 0;
     gameScoreOnGO();
-  }
-}
-}
+  }}}
 
 function damageAlien() {
   for (let i = 0; i < cellCount ; i++) {
@@ -98,8 +79,7 @@ function damageAlien() {
     let result = lasersPositionsArray.indexOf(i);
     lasersPositionsArray.splice(result, 1);
     gameScoreOnGO();
-} 
-}}
+} }}
 
 
 function makeGrid() { // make grid map
@@ -119,13 +99,12 @@ if (localStorage.getItem("level") > 1) {
   button1.classList.add('button1');
   button1.innerHTML = 'PLAY';
   grid.appendChild(button1); 
-}
-}
+}}
 
 function createRocks() { //create all the rocks
   for (let i = 1; i <  3; i++) { //add rocks
     cells[cellCount - (width*3 + 7*i)].classList.add('rocksClass'); //creates 1st set of aliens
-    rocksPositions.push(cellCount - 5*i);
+    rocksPositions.push(cellCount - (width*3 + 7*i));
   }}
 
 
@@ -139,14 +118,6 @@ function createAliens(x) { //create all the aliens
   cells[2 + i * 2 + width * 2].classList.add('alienShip3'); // creates 3rd set of aliens
   aliensPosArray3.push(2 + i * 2 + width * 2);
   }}
-
-  // cells[(4-x) + i * 2].classList.add('alienShip'); //creates 1st set of aliens
-  // aliensPosArray1.push((4-x) + i * 2);
-  // cells[(4-x) + i * 2 + width].classList.add('alienShip2'); //creates 2nd set of aliens
-  // aliensPosArray2.push((4-x) + i * 2 + width);
-  // cells[(4-x) + i * 2 + width * 2].classList.add('alienShip3'); // creates 3rd set of aliens
-  // aliensPosArray3.push((4-x) + i * 2 + width * 2);
-
 
 function moveAliensRight() {  //move Aliens right 1 space
   for (let i = 0; i < aliensPosArray1.length; i++){
@@ -182,7 +153,7 @@ function moveAliensLeft() {   //move Aliens Left 1 space
     cells[aliensPosArray3[i]].classList.add('alienShip3');
     }}
 
-function moveFourRight() {        //move aliens right 3 times every 600 miliseconds
+function moveFourRight() {        //move aliens right 3 times every 500 miliseconds
   let movement = 0;
   let moveAliens = setInterval(() => {
     if(movement < 4 && playerCurrentHealth > 0) {
@@ -190,11 +161,13 @@ function moveFourRight() {        //move aliens right 3 times every 600 miliseco
     movement ++;
      } else {
       clearInterval(moveAliens);
+      checkRocks();
+      gameScoreOnGO();
       moveFourLeft(); //alternate with move left
      }
 }, 500)}
 
-function moveFourLeft() {         //move aliens left 3 times every 600 miliseconds
+function moveFourLeft() {         //move aliens left 3 times every 500 miliseconds
   let movement1 = 0;
   let moveAliens = setInterval(() => {
     if(movement1 < 4 && playerCurrentHealth > 0) {
@@ -204,11 +177,15 @@ function moveFourLeft() {         //move aliens left 3 times every 600 milisecon
       clearInterval(moveAliens);
       moveFourRight(); //alternate with move right
       moveAliensDown();
+      checkRocks();
+      gameScoreOnGO();
 
      }
 }, 500)}
 
 function moveAliensDown() {           //moves aliens donwwards
+  let movemenet = 0;
+  if(movemenet < 16 && playerCurrentHealth > 0) { //# times + stops if player is dead
   for (let i = 0; i < aliensPosArray1.length; i++){ //for line 1 use array1.length
     cells[aliensPosArray1[i]].classList.remove('alienShip');
     aliensPosArray1[i] += width;
@@ -223,21 +200,11 @@ function moveAliensDown() {           //moves aliens donwwards
   cells[aliensPosArray3[i]].classList.remove('alienShip3');
   aliensPosArray3[i] += width;
   cells[aliensPosArray3[i]].classList.add('alienShip3');
-  }}
+  }
+
+  movemenet ++;
+}}
   
-function moveallthewayDown() { // move aliens down
-  let movemenet = 0;
-    if(movemenet < 18 && playerCurrentHealth > 0) {         //# times + stops if player is dead
-      moveAliensDown();
-      checkRocks();
-      gameScoreOnGO();
-    movemenet ++;
-     } else {
-       gameOver = 1;
-
-     }
-}          
-
 
 function addPlayerShip() {       
   if (playerCurrentHealth > 0) {                 //place playership
@@ -319,21 +286,15 @@ function moveLasers() {
       removeBomb(alienBombPosArray[i]); //initial bomb remove
       if ((alienBombPosArray[i] + width) > (cellCount - 1)) { //if next position will be offscreen => 
         removeBomb(alienBombPosArray[i]); // remove bomb
-    // } else if (allAliens.length > 0 && playerCurrentHealth > 0 && (cells[i].classList.contains('alienShip2') || cells[i].classList.contains('alienShip') || cells[i].classList.contains('alienShip3'))) { //if there are still aliens left & player is alive //experimental code
-    //   alienBombPosArray[i] += width * 2;  //updates location of the new bomb
-    //   createBomb(alienBombPosArray[i]); //adds alienBomb class to a cell div with the createBomb function.
     } else if (allAliens.length > 0 && playerCurrentHealth > 0){ //if there are still aliens left & player is alive //experimental code
         alienBombPosArray[i] += width;  //updates location of the new bomb
         createBomb(alienBombPosArray[i]); //adds alienBomb class to a cell div with the createBomb function.
-      }
-    }
-}, 170);
-  }
+      }}}, 170);}
 
 function dropBombs(bombsTiming) {           //time loop that drops bombs from aliens
   let dropingBombs = setInterval(() => {
       newBombInit();                //initiate a new bomb
-    }, bombsTiming) }                 //how often
+    }, bombsTiming)}                 //how often
 
 function levelChange(x) { //change level and restart game
   x ++;                                 //for each level change +1
@@ -350,11 +311,12 @@ function levelChange(x) { //change level and restart game
 
   function checkRocks() {
     for (let i = 0; i < cellCount; i++) {
-      if ((cells[i].classList.contains('alienShip') && cells[i].classList.contains('rocksClass')) || (cells[i].classList.contains('alienShip2') && cells[i].classList.contains('rocksClass')) || (cells[i].classList.contains('alienShip3') && cells[i].classList.contains('rocksClass')) ) {
+      if ((cells[i].classList.contains('alienShip') && cells[i].classList.contains('rocksClass')) 
+      || (cells[i].classList.contains('alienShip2') && cells[i].classList.contains('rocksClass')) 
+      || (cells[i].classList.contains('alienShip3') && cells[i].classList.contains('rocksClass')) ) {
         gameOver = 2;
-      }
-  }
-}
+        console.log('no?');
+      }}}
 
 function gameScoreOnGO() {          //displayig score on Game Over
   let allAliens = aliensPosArray1.concat(aliensPosArray2,aliensPosArray3);    
@@ -374,7 +336,6 @@ function gameScoreOnGO() {          //displayig score on Game Over
       }
       document.querySelector(".modal-content").innerHTML = "Level cleared, " + playerNameHtml.innerHTML + "! Your score is " + playerOneScore + "!"; //displayed text on win 
       document.querySelector(".modal-content").appendChild(nxtLvlBut); //button element being added to the modal
-
   } else if (gameOver === 1 && playerCurrentHealth <= 0) {
     modal.style.display = "block"; // show modal
       let nxtLvlBut = document.createElement("button"); //create button
@@ -387,9 +348,7 @@ function gameScoreOnGO() {          //displayig score on Game Over
       }
       document.querySelector(".modal-content").innerHTML = playerNameHtml.innerHTML + ", your ship was destroyed. Better luck next time. Your score is " + playerOneScore + "<br><br>"; //displayed text on win 
       document.querySelector(".modal-content").appendChild(nxtLvlBut); //button element being added to the modal
-
-      // window.confirm(playerNameHtml.innerHTML + ", you died. Better luck next time. Your score is " + playerOneScore) ? resetFun() : resetFun();
-        }
+      }
   if (gameOver === 2) {
     modal.style.display = "block"; // show modal
       let nxtLvlBut = document.createElement("button"); //create button
@@ -402,10 +361,7 @@ function gameScoreOnGO() {          //displayig score on Game Over
       }
       document.querySelector(".modal-content").innerHTML = playerNameHtml.innerHTML + ", you lost. Your planet has been invaded. Better luck next time. Your score is " + playerOneScore + "<br><br>"; //displayed text on win 
       document.querySelector(".modal-content").appendChild(nxtLvlBut); //button element being added to the modal
-
-    // window.confirm(playerNameHtml.innerHTML + ", you lost. Your planet has been invaded. Better luck next time. Your score is " + playerOneScore) ? resetFun() : resetFun();
-    }
-          }
+    }}
 
 function addEventListeners() {
 let detectSpacePress4Laser = document.addEventListener('keydown', (event) =>{ //spacebar hit detection
@@ -425,15 +381,12 @@ let playerMove = document.addEventListener('keydown', (event) =>{ //player move 
       }
       speedPos = playerPosit + width;
       addPlayerShip(); //
-});
-}
+});}
 
 function gameInit() {            //initiates games basically, calls all initial functions
   if (localStorage.getItem("level") != null) {
-    updateLvlOnStart(localStorage.getItem("level"),);
-  } //update level from local storage
+    updateLvlOnStart(localStorage.getItem("level"),);} //update level from local storage
   playerNameHtml.innerHTML = localStorage.getItem("playerName"); //update name from local storage
-
   makeGrid();
   bombsTimingUpdate(Number(currentLevel)); // update bombs timing to level
   moveLasers();
@@ -447,27 +400,9 @@ function gameInit() {            //initiates games basically, calls all initial 
   grid.style.cursor = 'none'; //hide cursor when game starts
 }   
 
-//custom prompt screen
-//scoreboard
-
 makeButtons();
-let button1 = document.querySelector('.button1');
 
-
-
-
-
-// Get the modal
-const modal = document.getElementById("myModal");
-
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-const modalForm = document.querySelector(".modalform");
-const submit = document.querySelector(".submit");
-
-// When the user submits, close the modal, set name, start game
-modalForm.onsubmit = function() {
+modalForm.onsubmit = function() { // When the user submits, close the modal, set name, start game
   playerNameHtml.innerHTML = document.forms["playerOneForm"]["pname"].value;
   localStorage.setItem("playerName", playerNameHtml.innerHTML);
   modal.style.display = "none"; // close modal
@@ -475,20 +410,15 @@ modalForm.onsubmit = function() {
   return false;
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function(event) { // When the user clicks anywhere outside of the modal, close it
   if (event.target == modal) {
-    console.log('noooo');
     playerNameHtml.innerHTML = document.forms["playerOneForm"]["pname"].value;
     localStorage.setItem("playerName", playerNameHtml.innerHTML);
     modal.style.display = "none"; // close modal
     gameInit(); //start game
-
-  }
-}
+  }}
 
 button1.onclick = function() {
   modal.style.display = "block"; // show modal
-  button1.remove();
-  // playerNameHtml.innerHTML = window.prompt('What is your name, player?', 'Player 1'); //updates player name from prompt
+  button1.remove(); //remove button
 }
